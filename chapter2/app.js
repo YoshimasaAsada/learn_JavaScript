b5e3ba88b03908dddb164d351a411d9761edf4f7
麻田祥正 <asadayoshimasa@asadayoshimasanoMacBook-Pro.local>
2022-07-02 16:29:11 +0900

    chapter2

---
+++ chapter2/app.js
@@ -0,0 +1,57 @@
+// let hello = "Hello World";
+
+// let int = 1;
+// alert(int);
+
+// let int2 = -10;
+// alert(int2);
+
+// let float = 3.14;
+// alert(float);
+
+// let str1 = "JavaScriptを学ぼう";
+// alert(str1);
+
+// alert(4 + 3);
+
+// alert(8 - 5);
+
+// alert(2 * 6);
+
+// alert(10 / 2);
+
+// alert("Hello" + "World");
+
+// let str1 = "Hello";
+// let str2 = "World";
+// alert(str1 + str2);
+
+// let orange = 100;
+// let apple = 19;
+//  if(orange < apple) {
+//    alert("みかんの値段はりんごより安い");
+//  } else if(orange == apple) {
+//    alert("みかんとりんごは同じ値段");
+//  } else {
+//    alert("みかんの値段がりんごより高い");
+//  }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max) {
+//   num = num * 2
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに使った回数は' + count + '回です');
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足した数は' + num + "です");
