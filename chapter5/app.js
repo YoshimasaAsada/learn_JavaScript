6070f066725a85d0eb11731f4bee4e88150e7f47
麻田祥正 <asadayoshimasa@asadayoshimasanoMacBook-Pro.local>
2022-07-03 10:56:46 +0900

    chapter5

---
+++ chapter5/app.js
@@ -0,0 +1,13 @@
+// $(function(){
+//   $('.box1').hide();
+// });
+
+$(function (){
+  $('.box1').slideDown(100,function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
+});
